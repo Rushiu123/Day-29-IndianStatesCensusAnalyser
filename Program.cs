829d656c1c19eb35e0225b54de43f3ac03a948ae
Rushiu123 <rushikesh.ubalkar@gmail.com>
2023-02-07 18:35:21 +0530

    IndianStatesCensusAnalyserAdd

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace IndianStatesCensusAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-=Welcome To The Indian State Census Analyser Problem=-=-=-=-=-=-=-=");
+            Console.ReadLine();A
+        }
+    }
+}
